5da6137bd2afeebc8ecaaa255a679654c3d822eb
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-08-09 21:57:51 +0530

    temperature unit conversion

---
+++ src/hooks/UseTempUnit.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+
+export const useTempUnit = () => {
+
+    const [unitMode,setUnitMode] = useState(false); // false = C, true = F
+
+    //toggle unit mode
+    const unitModeHandler = () => {
+        setUnitMode(!unitMode);
+        localStorage.setItem('unitMode',!unitMode); // save unit mode to local storage
+    }
+
+    useEffect(() => {
+        //check if unit mode is saved in local storage
+        //if not, save unit mode to local storage
+        //else, set unit mode to the value in local storage
+        if(JSON.parse(localStorage.getItem('unitMode')) === null){
+            localStorage.setItem('unitMode',unitMode);
+    } else{       
+        setUnitMode(unitMode);
+    } },[]);
+    return [unitMode,unitModeHandler];
+
+}
